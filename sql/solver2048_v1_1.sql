58fca8294e8d92074934ad7ed8dc44bc345c83f8
Mathijs Miermans <m.miermans@gmail.com>
2014-07-23 21:24:42 +0200

    Database structure is now an empty.

---
+++ sql/solver2048_v1_1.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 23, 2014 at 07:44 PM
+-- Server version: 5.5.38-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `solver2048`
+--
+CREATE DATABASE IF NOT EXISTS `solver2048` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `solver2048`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `games`
+--
+
+CREATE TABLE IF NOT EXISTS `games` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `start_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'The default value is a hack to make it now() as default for MySQL 5.5 (only one CURRENT_TIMESTAMP column allowed).',
+  `end_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `score` int(11) NOT NULL DEFAULT '0' COMMENT 'Score as calculated per rules of 2048.',
+  `max_tile` int(11) NOT NULL DEFAULT '0' COMMENT 'Value of highest tile.',
+  `has_ended` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'True if the game has reached a deadlocked position.',
+  `board` bigint(20) unsigned zerofill NOT NULL DEFAULT '00000000000000000000' COMMENT 'Board position encoded in 64-bit integer.',
+  `move_count` int(11) NOT NULL DEFAULT '0' COMMENT 'Number of moves so far including this move.',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Stores game state and statistics' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `moves`
+--
+
+CREATE TABLE IF NOT EXISTS `moves` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `game_id` bigint(20) NOT NULL,
+  `board_before_move` bigint(20) unsigned zerofill NOT NULL DEFAULT '00000000000000000000' COMMENT 'Board before move has been executed.',
+  `move_direction` enum('up','right','down','left') NOT NULL COMMENT 'Move that player performed on board (before new tile was inserted).',
+  `move_count` int(11) NOT NULL,
+  `new_tile_value` int(11) NOT NULL DEFAULT '2',
+  `new_tile_position` int(11) NOT NULL DEFAULT '0' COMMENT 'Position encoded as x+4y.',
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `game_id` (`game_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Information regarding individual moves.' AUTO_INCREMENT=1 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `moves`
+--
+ALTER TABLE `moves`
+  ADD CONSTRAINT `moves_game_id` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
